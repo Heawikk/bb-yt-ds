084991ecc391cf705d647dfb2b618f9ce5b4d3a4
Heawik <132798471+Heawikk@users.noreply.github.com>
2025-04-06 01:43:51 -0700

    1.0.1
    - Removed srv_install.bat srv_status.bat & ipsets in one file
    - Added new ipsets and domains

--- srv_remove.bat
+++ srv_remove.bat
@@ -1,21 +1,20 @@
 @echo off
-chcp 65001 >nul
-:: 65001 - UTF-8
+chcp 65001 > nul
 
-set "arg=%1"
-if "%arg%" == "admin" (
-    echo Restarted with admin rights
+if "%1"=="admin" (
+    echo Started with admin rights
 ) else (
+    echo Requesting admin rights...
     powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs"
     exit /b
 )
 
-set SRVCNAME=zapret
-
+set SRVCNAME=bbytds
 net stop %SRVCNAME%
 sc delete %SRVCNAME%
-
 net stop "WinDivert"
 sc delete "WinDivert"
 net stop "WinDivert14"
 sc delete "WinDivert14"
+pause
+endlocal
